257c920ebd77189bb0bc8b121e1ef1b57c335198
Natalie Tapias <nataliemtapias@gmail.com>
2019-11-09 15:35:53 -0700

    recursive method digit match

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -78,8 +78,8 @@ def search(array, value)
     search(array, value)
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) it depends on the length of the array
+# Space complexity: O(1) constant; there isn't any new array in memory
 def is_palindrome(s, i = 0, j = -1)
   if i == (s.length / 2)
     return true
@@ -92,8 +92,72 @@ def is_palindrome(s, i = 0, j = -1)
   is_palindrome(s, i, j)
 end
 
-# # Time complexity: ?
-# # Space complexity: ?
-# def digit_match(n, m)
-#     raise NotImplementedError, "Method not implemented"
+# Time complexity: ?
+# Space complexity: ?
+
+# def digit_match(n, m, c = 0, i = 0)
+#   # go to an array if not already
+  
+#   if n.class != Array && m.class != Array
+#     n = n.to_s.split("")
+#     m = n.to_s.split("")
+#   end
+
+#   if n.length == m.length
+#   elsif n.length > m.length
+#     extra_digits = (n.length) - (m.length)
+#     extra_digits.times do 
+#       m.insert(0, 0)
+#     end
+#   elsif n.length < m.length
+#     extra_digits = (m.length) - (n.length)
+#     extra_digits.times do 
+#       n.insert(0, 0)
+#     end
+#   end
+
+#   if i == n.length
+#     return c
+#   end
+
+#   if n[i] == m[i]
+#     c += 1
+#   end
+
+#   i += 1
+
+#   digit_match(n, m, c, i)
+
 # end
+
+# another approach is to think about going backwards 
+def digit_match(num1, num2, i = -1, c = 0)
+
+    num1 = num1.to_s
+    num2 = num2.to_s
+
+
+  if num1.length == num2.length
+    if i.abs == num2.length + 1
+      return c
+    end
+  elsif num1.length > num2.length
+    if i.abs == num2.length + 1
+      return c
+    end
+  elsif num1.length < num2.length
+    if i.abs == num2.length + 1
+      return c
+    end
+  end
+
+  if num1[i] == num2[i]
+    c += 1
+  end
+
+  i -= 1
+
+  digit_match(num1, num2, i, c)
+end
+
+
