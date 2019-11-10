7034c2bd90b262eba118dc724dafb1a6510c4b23
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-11-09 19:32:32 -0800

    First five methods created

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -3,31 +3,67 @@
 # Time complexity: ?
 # Space complexity: ?
 def factorial(n)
-    raise NotImplementedError, "Method not implemented"
+  if n < 0
+    raise ArgumentError
+  end
+  if n <= 1
+    return 1
+  else
+    n * factorial(n - 1)
+  end
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def reverse(s)
-    raise NotImplementedError, "Method not implemented"
+  if s.length > 1
+    first_char = s[0]
+    last_char = s[-1]
+    s = reverse(s.delete_prefix(s[0]).delete_suffix(s[-1]))
+    return last_char + s + first_char
+  else
+    return s
+  end
 end
 
 # Time complexity: ?
 # Space complexity: ?
-def reverse_inplace(s)
-    raise NotImplementedError, "Method not implemented"
+def reverse_inplace(s, index = 1)
+  if index <= s.length/2
+    first_char = s[index-1]
+    last_char = s[-index]
+    s[index-1] = last_char
+    s[-index] = first_char
+    return reverse_inplace(s, index += 1)
+  else
+    return s
+  end
 end
 
 # Time complexity: ?
 # Space complexity: ?
-def bunny(n)
-    raise NotImplementedError, "Method not implemented"
+def bunny(n, ears = 0)
+  if ears == n + n
+    return ears
+  else
+    ears += 2
+    return bunny(n, ears)
+  end
 end
 
 # Time complexity: ?
 # Space complexity: ?
-def nested(s)
-    raise NotImplementedError, "Method not implemented"
+def nested(s, i = 1)
+  if s.length % 2 != 0
+    return false
+  end
+  if i > s.length / 2 || s.length == 0
+    return true
+  elsif s[i-1] == "(" && s[-i] == ")"
+    return nested(s, i += 1)
+  else
+    return false
+  end
 end
 
 # Time complexity: ?
