1360368fc84477dc9f38b465966ddbb0f1f30c78
stupendousC <CarolineWuKaplan@gmail.com>
2019-11-09 16:30:52 -0800

    1 to go

--- lib/cw.rb
+++ lib/cw.rb
@@ -1,26 +1,17 @@
-def digit_match(n, m)
-  # puts "digit_matching #{n} vs #{m}"
-  # puts "comparing #{n%10} vs #{m%10}"
+def fib(n)
+  # returns the nth fibonacci number
+  # e.g. fib(4) = (0 1 1 2 3) should return 3
+  # Try it with a large number (> 100), what do you notice happening?
+  # it got a lot slower as n increases, really noticeable around n=40
   
-  if n>9 && m>9
-    # recurse-able condition
-    if n%10 == m%10
-      return 1 + digit_match(n/10,m/10)
-    else
-      return 0 + digit_match(n/10, m/10)
-    end
-    
+  if n == 0
+    return 0
+  elsif n == 1
+    return 1
   else
-    # no need to recurse, n and/or m already on single digits
-    if n%10 == m%10
-      return 1
-    else
-      return 0
-    end
+    # generate fib seq up to the n-th index place
+    return fib(n-1) + fib(n-2)
   end
-  
 end
 
-n = 54321
-m = 12345
-p digit_match(n,m)
+
--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -157,10 +157,29 @@ end
 
 
 # Added Fun
+# Time complexity: O(n*2^n), almost I think.  
+# It'll go n-1 levels deep, with up to 2^n leaves, so n*2^n...
+# Space complexity: O(n*2^n)...?  not sure
 def fib(n)
   # returns the nth fibonacci number
   # e.g. fib(4) = (0 1 1 2 3) should return 3
   # Try it with a large number (> 100), what do you notice happening?
+  # it got a lot slower as n increases, really noticeable around n=40
   
-  
+  if n == 0
+    return 0
+  elsif n == 1
+    return 1
+  else
+    # generate fib seq up to the n-th index place
+    return fib(n-1) + fib(n-2)
+  end
 end
+
+### to see fib(n) in action, uncomment chunk below
+# n = 0
+# while n < 200
+#   puts "\n\nn = #{n}"
+#   p fib(n)
+#   n+= 1
+# end
