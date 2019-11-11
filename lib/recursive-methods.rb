0e73782e15ab246fea080c1e78401c1f0d930774
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-11-11 11:19:32 -0800

    Passing digit_match tests, whoo hoo.

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -13,8 +13,8 @@ def factorial(n)
   end
 end
 
-# Time complexity: O of n where n is the length of the string.
-# Space complexity: O of n.
+# Time complexity: Since the recursive step is acting on two elements at a time, it's O of n/2 for even string lengths and O of n/2 + 1 for odd lengths where n is the integer length of the string.
+# Space complexity: Same as time complexity. 
 def reverse(s)
   # nil case and base case
   if s.length == 0 || s.length == 1
@@ -25,8 +25,9 @@ def reverse(s)
   end
 end
 
-# Time complexity: O of n where n is the length of the string.
-# Space complexity: O of n.
+# Time complexity: Same as above -- O of n/2 for even string lengths and O of n/2 + 1 where n is the length of the string.
+# Space complexity: Same as time complexity.
+# Note: I think this is reversing in place because we're not creating any new variables?
 def reverse_inplace(s)
   # nil case and base case
   if s.length == 0 || s.length == 1
@@ -49,8 +50,8 @@ def bunny(n)
   end
 end
 
-# Time complexity: O of n where n is the number of parens in the string (possibly minus one or two)
-# Space complexity: O of n
+# Time complexity: O of n/2 where n is an even number of parens in the string, O of n/2 + 1 when the string character count is odd. 
+# Space complexity: Same as time complexity. 
 def nested(s)
   if s.length == 0
     return true
@@ -85,16 +86,16 @@ def search(array, value)
   return false
 end
 
-# Time complexity: O of n
-# Space complexity: O of n
+# Time complexity: O of (n/2) for even counts or (n/2) + 1 for odd where n is the length of the string ... reduced to just O of n since we drop the coefficients.
+# Space complexity: Same as time complexity
 def is_palindrome(s)
   # base case 1
   if s.length == 0 || s.length == 1
     return true
-  # base case 2
+    # base case 2
   elsif s.length == 2 && s[0] == s[-1]
-      return true
-  # base case 3?
+    return true
+    # base case 3?
   elsif s[0] != s[-1]
     return false
   else
@@ -105,8 +106,25 @@ def is_palindrome(s)
   end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O of n/2 (n/2 + 1 if the length is odd)
+# Space complexity: Same as time complexity
 def digit_match(n, m)
-  raise NotImplementedError, "Method not implemented"
+  # make integers strings so we can use string methods on them
+  n = n.to_s
+  m = m.to_s
+  # base case 1: there are no characters to compare
+  if n.length == 0 || m.length == 0
+    return 0
+  # base case 2: the last characters of each string match
+  else
+    if n[-1] == m[-1]
+      match = 1
+    else
+      match = 0
+    end
+    n.slice!(-1)
+    m.slice!(-1)
+  # recursive step: return match and both strings (with last character removed)
+  return match + digit_match(n, m)
+  end
 end
