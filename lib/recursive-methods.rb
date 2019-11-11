e843206ddb8330fb3f1b8f91495de668aa825857
dora1405 <li.dora05@gmail.com>
2019-11-10 20:46:37 -0800

    digit_match recursion complete. All tests passing

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -4,6 +4,7 @@
 # Space complexity: O(n)
 def factorial(n)
   raise ArgumentError if n < 0
+
   return 1 if n == 0
   return n * factorial(n-1)
 end
@@ -12,8 +13,10 @@ end
 # Space complexity: O(n)
 def reverse(s)
   return s if s.length <= 1
+
   new_string = reverse(s[1..-1])
   new_string += s[0]
+
   return new_string
 end
 
@@ -37,7 +40,7 @@ def nested(s)
   return true if s.empty?
   return false if s.length.odd?
   return false if s[0] == s[-1]
-  nested(s[1..-2])
+  return nested(s[1..-2])
 end
 
 # Time complexity: O(n)
@@ -45,20 +48,30 @@ end
 def search(array, value)
   return false if array.empty?
   return true if array[0] == value
-  search(array[1..-1], value)
+  return search(array[1..-1], value)
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def is_palindrome(s)
   return true if s.length <= 1
   return false if s.length.even?
   return true if s[0] == s[-1]
-  is_palindrome(s[1..-2])
+  return is_palindrome(s[1..-2])
 end
 
-# Time complexity: ?
-# Space complexity: ?
-def digit_match(n, m)
-  raise NotImplementedError, "Method not implemented"
+# Time complexity: O(n)
+# Space complexity: O(n)
+def digit_match(n, m, sum = 0)
+  n = n.to_s
+  m = m.to_s
+
+  return sum += 1 if (n.length == 1 || m.length == 1) && n[-1] == m[-1]
+  return sum if n.length == 1 || m.length == 1
+
+  if n[-1] == m[-1]
+    sum += 1
+  end
+
+  return digit_match(n[0...-1], m[0...-1], sum)
 end
