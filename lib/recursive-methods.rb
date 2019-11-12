e3ae03a0fed089a1adf93eeec5ac0edf5d5cfc22
Vi <vanessareigosa@gmail.com>
2019-11-11 19:55:30 -0800

    6/8 complete, two left

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -1,27 +1,52 @@
 # Authoring recursive algorithms. Add comments including time and space complexity for each method.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def factorial(n)
-    raise NotImplementedError, "Method not implemented"
+  if n < 0 
+    raise ArgumentError
+  elsif n <= 1 
+    return 1 
+  else
+    return n * factorial(n - 1)
+  end 
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n)
 def reverse(s)
-    raise NotImplementedError, "Method not implemented"
+  reversed_string = ""
+  if s.length >= 1 
+    reversed_string = reverse(s[1..-1])
+    reversed_string += s[0]
+  else 
+    return reversed_string
+  end
+end 
+
+# Time complexity: O(n)
+# Space complexity: O(1)
+def reverse_inplace(s, i = 0, j = -1)
+  n = s.length 
+  if i == n / 2 
+    return s 
+  else 
+    letter = s[i]
+    s[i] = s[j]
+    s[j] = letter 
+    reverse_inplace(s, i + 1, j - 1)
+  end 
 end
 
-# Time complexity: ?
-# Space complexity: ?
-def reverse_inplace(s)
-    raise NotImplementedError, "Method not implemented"
-end
-
-# Time complexity: ?
-# Space complexity: ?
-def bunny(n)
-    raise NotImplementedError, "Method not implemented"
+# Time complexity: O(n)
+# Space complexity: O(n)
+def bunny(n, ears = 0)
+  if ears == n + n 
+    return ears
+  else
+    ears += n
+    bunny(n, ears)
+  end 
 end
 
 # Time complexity: ?
@@ -30,16 +55,33 @@ def nested(s)
     raise NotImplementedError, "Method not implemented"
 end
 
-# Time complexity: ?
+# Time complexity: O(n)
 # Space complexity: ?
 def search(array, value)
-    raise NotImplementedError, "Method not implemented"
-end
+  new_array_minus_one = 0..-2
 
-# Time complexity: ?
-# Space complexity: ?
-def is_palindrome(s)
-    raise NotImplementedError, "Method not implemented"
+  if array.empty?  
+    return false 
+  elsif array[-1] == value
+    return true 
+  else 
+    search(array[new_array_minus_one], value)
+  end 
+end 
+
+# Time complexity: O(n)
+# Space complexity: O(1) 
+def is_palindrome(s, i = 0, j = -1)
+  n = s.length 
+
+  if i >= s.length + j
+    return true
+  elsif
+    s[i] != s[j]
+    return false
+  elsif s[i] == s[j]
+    is_palindrome(s, i + 1, j - 1)
+  end 
 end
 
 # Time complexity: ?
