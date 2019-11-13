d610d977285d91b037ba2f8ab19b3314697da1b9
Yasmin <yasmin312002@hotmail.com>
2019-11-12 22:14:56 -0800

    tests passing for waves 1,2,4,5,6

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -1,40 +1,75 @@
 # Authoring recursive algorithms. Add comments including time and space complexity for each method.
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: o(n)
+# Space complexity: o(n)
 def factorial(n)
-    raise NotImplementedError, "Method not implemented"
+    # raise NotImplementedError, "Method not implemented"
+  if n < 0
+    raise ArgumentError
+  elsif n <= 1
+    return  1
+  else 
+    n * factorial(n-1)
+  end 
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: o(n^2)
+# Space complexity: o(n^2)
 def reverse(s)
-    raise NotImplementedError, "Method not implemented"
+    # raise NotImplementedError, "Method not implemented"
+  if s.length <= 1
+    return s
+  else 
+    return s[-1] + reverse(s[0..-2]) 
+  end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: 
+# Space complexity: 
 def reverse_inplace(s)
     raise NotImplementedError, "Method not implemented"
+ 
 end
 
-# Time complexity: ?
-# Space complexity: ?
+
+# Time complexity: o(n)
+# Space complexity: o(n)
 def bunny(n)
-    raise NotImplementedError, "Method not implemented"
+    # raise NotImplementedError, "Method not implemented"
+  if n == 0
+    return 0
+  else
+    return 2 + bunny(n-1)
+  end
 end
 
-# Time complexity: ?
-# Space complexity: ?
+
+# Time complexity: o(n)
+# Space complexity: o(n^2)
 def nested(s)
-    raise NotImplementedError, "Method not implemented"
+    # raise NotImplementedError, "Method not implemented"
+  if s == ""
+    return true
+  elsif s[0] == "(" && s[-1] == ")" && nested(s[1...-1])
+     return true
+  else 
+    return false
+  end
 end
 
 # Time complexity: ?
 # Space complexity: ?
 def search(array, value)
-    raise NotImplementedError, "Method not implemented"
+    # raise NotImplementedError, "Method not implemented"
+  if array.length == 0
+    return false
+  elsif value == array[0]
+    return true
+  else 
+    return search(array[1..-1], value)
+  end
 end
+
 
 # Time complexity: ?
 # Space complexity: ?
