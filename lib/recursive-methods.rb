6d404547a295068e0fe1a73e1f6b6a60a65c432e
Diana <dianah46@gmail.com>
2019-11-15 18:52:01 -0800

    need some help with nested and digit_match

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -1,5 +1,5 @@
 # Authoring recursive algorithms. Add comments including time and space complexity for each method.
-
+require 'pry'
 # Time complexity: O(n)
 # Space complexity: O(n)
 def factorial(n)
@@ -34,24 +34,52 @@ end
 
 # Time complexity: ?
 # Space complexity: ?
+# NOT PASSING TESTS
 def nested(s)
   return true if s.length == 0
+  # need to check if s = even number of paren-pairs()
+  # return true if s.length % 2 == 0
+end
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+def search(array, value, i = 0)
+  return false if i >= array.length
+  return true if array[i] == value
+  return search(array, value, i + 1)
+end
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+# ONLY PASSING 2 TESTS
+def is_palindrome(s, l = 0, r = s.length-1 )
+  if s[l] == s[r]
+    return true
+  else
+    return false
+  end
+
+  while l < s.length
+    return is_palindrome(s, l + 1, r - 1)
+  end
 end
 
 # Time complexity: ?
 # Space complexity: ?
-def search(array, value)
-    raise NotImplementedError, "Method not implemented"
-end
+# NOT PASSING TESTS
+def digit_match(n, m, i = -1)
+# i want to check each index from the end and keep track of matching values
+  n = n.to_s
+  m = m.to_s
+  tracker = 0
 
-# Time complexity: ?
-# Space complexity: ?
-def is_palindrome(s)
-    raise NotImplementedError, "Method not implemented"
-end
+  if n[i] == m[i]
+    tracker += 1
+  else
+    tracker += 0
+  end
 
-# Time complexity: ?
-# Space complexity: ?
-def digit_match(n, m)
-    raise NotImplementedError, "Method not implemented"
+  return tracker if i < -(n.length)
+
+  return digit_match(n, m, i - 1)
 end
