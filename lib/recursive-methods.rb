97f7c9c259468e0611ad1344023412efec568b47
Erika Maust <jamjar@Erikas-MacBook-Pro.local>
2020-07-01 11:33:43 -0700

    factorial and reverse string (not in place) written

--- lib/recursive-methods.rb
+++ lib/recursive-methods.rb
@@ -1,65 +1,61 @@
-# Authoring recursive algorithms. Add comments including time and space complexity for each method.
+# # Authoring recursive algorithms. Add comments including time and space complexity for each method.
 
-# Time complexity: ?
-# Space complexity: ?
-def factorial(n)
-  if n == 0 || n == 1
-    return 1
-  elsif n < 0
-    raise ArgumentError
-  end
-  return n * factorial(n-1)
-  # return times(n, factorial(n-1))
-end
+# # Time complexity: O(n)
+# # Space complexity: O(n)
+# def factorial(n)
 
+#   raise ArgumentError unless n >= 0 
+#   return 1 if n == 1 || n == 0
+
+#   n * factorial(n - 1)
+
+# end
 
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(n) - I think O(n) because n amount of memory will be used to create the stack during the recursive call. I am creating a variable, but only concat. onto the existing one (<<) rather than creating a new variable each time (+=) so I think the memory allocation for the variable is O(1).
+
 def reverse(s)
-    if s.length == 1 || s == ""
-      return s
-    else
-      return reverse(s[1..-1]) + s[0]
-    end
+
+  return s if s.length <= 1
+  reversed = reverse(s[1..-1])
+  reversed << s[0]
+
 end
 
-# Time complexity: ?
-# Space complexity: ?
-def reverse_inplace(s)
-  if s.length == 1 || s == ""
-    return s
-  else
-    return reverse(s[1..-1]) + s[0]
-  end
-end
 
-# Time complexity: ?
-# Space complexity: ?
-def bunny(n)
-    raise NotImplementedError, "Method not implemented"
-end
+# # Time complexity: ?
+# # Space complexity: ?
+# def reverse_inplace(s)
+#   raise NotImplementedError, "Method not implemented"
+# end
 
-# Time complexity: ?
-# Space complexity: ?
-def nested(s)
-    raise NotImplementedError, "Method not implemented"
-end
+# # Time complexity: ?
+# # Space complexity: ?
+# def bunny(n)
+#     raise NotImplementedError, "Method not implemented"
+# end
 
-# Time complexity: ?
-# Space complexity: ?
-def search(array, value)
-    raise NotImplementedError, "Method not implemented"
-end
+# # Time complexity: ?
+# # Space complexity: ?
+# def nested(s)
+#     raise NotImplementedError, "Method not implemented"
+# end
 
-# Time complexity: ?
-# Space complexity: ?
-def is_palindrome(s)
-    raise NotImplementedError, "Method not implemented"
-end
+# # Time complexity: ?
+# # Space complexity: ?
+# def search(array, value)
+#     raise NotImplementedError, "Method not implemented"
+# end
 
-# Time complexity: ?
-# Space complexity: ?
-def digit_match(n, m)
-    raise NotImplementedError, "Method not implemented"
-end
+# # Time complexity: ?
+# # Space complexity: ?
+# def is_palindrome(s)
+#     raise NotImplementedError, "Method not implemented"
+# end
+
+# # Time complexity: ?
+# # Space complexity: ?
+# def digit_match(n, m)
+#     raise NotImplementedError, "Method not implemented"
+# end
